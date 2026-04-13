9ae5adea5bc83a71117916d4e5a749da34511c2d
outlandi-co <outlandico@gmail.com>
2026-04-13 03:19:36 -0700

    fix: socket connection for production (render)

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client"
 
 const socket = io(import.meta.env.VITE_API_URL, {
-  transports: ["websocket"]
+  transports: ["websocket", "polling"],
+  withCredentials: true
 })
 
 export default socket
