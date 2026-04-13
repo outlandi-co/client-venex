af821e49384aae493b5daa2a7af34f36a4433373
outlandi-co <outlandico@gmail.com>
2026-04-13 03:22:28 -0700

    fix: socket fallback for render

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client"
 
 const socket = io(import.meta.env.VITE_API_URL, {
-  transports: ["websocket", "polling"],
-  withCredentials: true
+  transports: ["polling", "websocket"], // 👈 polling FIRST
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000
 })
 
 export default socket
